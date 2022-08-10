e054de30b3b6f4fb6db7b657429e51f4c57c39b2
lucas <lucas@DESKTOP-JFUAC4T>
2022-08-10 16:24:10 +0800

    should be done may need to change smth

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,8 +1,1 @@
-import java.util.ArrayList;
 
-public class C206_CaseStudy {
-	
-	public static void main(String[] args) {
-		//commited by trina1
-	}
-}
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,10 +1,14 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+
+	public static String removeQutation;
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,6 +22,11 @@ public class C206_CaseStudyTest {
 	public void c206_test() {
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
+	}
+
+	public static String retrieveAllQuotation(ArrayList<String> quotationList) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
