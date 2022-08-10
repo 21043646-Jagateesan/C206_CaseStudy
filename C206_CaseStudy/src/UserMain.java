0b349297a1d9f22ac163d9f05d1c2331e83350e0
trina <trina@DESKTOP-DHM07L7>
2022-08-10 21:26:16 +0800

    UserMain(complete)

--- C206_CaseStudy/src/UserMain.java
+++ C206_CaseStudy/src/UserMain.java
@@ -95,7 +95,7 @@ public class UserMain {
 	}
 	
 	public static void addUser(ArrayList<User> customerList, User user) {
-		if (C206_CaseStudy.testExists(customerList, user.getName()) == false) {
+		if (UserMain.testExists(customerList, user.getName()) == false) {
 			customerList.add(user);
 			System.out.println("User account added!");
 		} else {
